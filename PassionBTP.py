6dcc4d896bdd742b7a75a5d5b9065e4be1ba125c
Reykiu <98109098+Reykiu@users.noreply.github.com>
2022-01-20 16:41:55 +0100

    Add files via upload

---
+++ PassionBTP.py
@@ -0,0 +1,78 @@
+# Importation   #HUGO , sully , mathias
+import random
+from dessin.codecache import rectangle
+from dessin.codecache import rectangle2
+from dessin.codecache import triangle
+from dessin.codecache import arc_de_cercle
+from dessin.codecache import carre
+
+ 
+# Fonction gestion des données
+def determiner_immeuble(numero):
+    reponse = {}
+    reponse['couleur_facade'] = 'red'
+    reponse['numero'] = numero
+    reponse['nombre'] = random.randint(1, 5)
+    return reponse
+ 
+# Fonctions d'interface graphique
+ 
+def dessiner_facade(informations):
+    facade = {}
+    facade['écriture'] = 'black'
+    facade['fond'] = informations['couleur_facade']
+    facade['épaisseur'] = 2
+    x = informations['numero']*180 - 300
+    y = informations['numero']*0
+    rectangle(140, random.choice([160, 240, 320, 400, 480]), facade, (x,y))
+    
+def dessiner_porte(informations):
+    porte = {}
+    porte['écriture'] = 'black'
+    porte['fond'] = informations['couleur_facade']
+    porte['épaisseur'] = 2
+    
+def dessiner_immeuble(informations:dict):
+    dessiner_facade(informations)
+    
+def stockX(numero):
+    '''Prends la clé 'numero' dans le dictionnaire reponse de la fonction determiner_immeuble'''
+    x0 = numero * 200 - 200
+    y0 = 0
+    return (x0, y0)
+    
+# Programme principal
+ 
+for x in range(4):
+    informations = determiner_immeuble(x)
+    dessiner_immeuble(informations)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ codecache.py
@@ -0,0 +1,196 @@
+'''Ce fichier permet de dessiner deux formes à l'aide des deux fonctions suivantes
+ 
++ triangle(cote, infos, coordonnees)
++ arc_de_cercle(rayon, angle, infos, coordonnees)
+ 
+Exemples d'utilisation :
+>>> divers1 = {'écriture':'blue', 'fond':'#FF88FF', 'épaisseur':5}
+>>> triangle(50, divers1, (50,100))
+>>> arc_de_cercle(75, 360, divers1, (200,-200))
+ 
+'''
+ 
+# Importation
+ 
+import turtle as trt
+import random as rd
+ 
+# Pas de classes
+ 
+# Déclaration des fonctions qui ne sont pas dans l'interface-programmation
+ 
+def nouveau_stylo(ecriture, fond, largeur):
+    '''Renvoie la référence d'un stylo configuré
+ 
+    :: param ecriture(str)  :: la couleur d'écriture ('red', '#FF0000')
+    :: param fond(str)      :: la couleur de fond pour ce stylo
+    :: param largeur(int)   :: la largeur du trait
+    :: return (Turtle)      :: renvoie un objet de la classe Turtle
+ 
+    '''
+    feutre = trt.Turtle()
+    feutre.color(ecriture)
+    feutre.fillcolor(fond)
+    feutre.pensize(largeur)
+    feutre.speed(5)
+    return feutre
+ 
+def deplacer(feutre, x, y):
+    '''Lève le feutre, déplace le feutre et abaisse le feutre
+ 
+    :: param feutre(Turtle) :: la référence du crayon
+    :: param x(int)         :: coordonnée horizontale (abscisse)
+    :: param y(int)         :: coordonnée verticale (ordonnée)
+    :: return (None)        :: c'est une fonction sans retour
+    .. effet de bord        :: modifie l'état de feutre
+ 
+    '''
+    feutre.penup()       # On lève la pointe
+    feutre.setpos(x, y)  # On déplace le crayon
+    feutre.pendown()     # On abaisse la pointe
+ 
+    
+def trace_triangle(feutre, largeur, hauteur, x, y):
+    feutre.begin_fill()
+    feutre.goto(x+largeur, y)
+    feutre.goto(x+largeur//2, y+hauteur)
+    feutre.goto(x, y)
+    feutre.end_fill()
+    feutre.hideturtle()
+    
+def trace_carre(feutre, cote):
+    feutre.begin_fill()
+    for x in range(4):
+        feutre.forward(cote)
+        feutre.left(90)
+    feutre.end_fill()
+    feutre.hideturtle()
+    
+    
+def trace_rectangle(feutre, largeur, hauteur):
+    feutre.begin_fill()
+    for x in range(2):
+        feutre.forward(largeur)
+        feutre.left(90)
+        feutre.forward(hauteur)
+        feutre.left(90)
+    feutre.end_fill()
+    feutre.hideturtle()
+    
+def trace_rectangle2(feutre, largeur, hauteur):
+    feutre.begin_fill()
+    for x in range(2):
+        feutre.forward(largeur)
+        feutre.left(90)
+        feutre.forward(hauteur)
+        feutre.left(90)
+    feutre.end_fill()
+    feutre.hideturtle()
+    
+def trace_arc(feutre, rayon, angle):
+    '''Trace un arc de cercle à l'aide du crayon feutre
+ 
+    :: param ftr(Turtle)    :: la référence du crayon
+    :: param rayon(int)     :: la valeur en pixel du rayon
+    :: param angle(int)     :: l'angle à tracer (360 pour un cercle)
+    :: return (None)        :: fonction sans retour
+    .. effet de bord        :: modifie l'état de ftr
+ 
+    '''
+    feutre.begin_fill()
+    feutre.circle(rayon, angle)
+    feutre.end_fill()
+    feutre.hideturtle()
+ 
+# Déclarations des fonctions d'interface-programmation
+ 
+ 
+def arc_de_cercle(rayon, angle, infos, coordonnees):
+    '''Trace un arc de triangle à partir des infos et aux bonnees coordonnées
+ 
+    :: param rayon(int)                    :: la valeur en pixel du rayon
+    :: param angle(int)                    :: la valeur en ° de l'angle
+    :: param infos(dict)                   :: un dictionnaire {"écriture":str, "fond":str, "épaisseur":int}    
+    :: param coordonnees(tuple (int,int) ) :: un tuple (x,y)
+ 
+    '''
+    ecriture = infos['écriture']
+    fond = infos['fond']
+    epaisseur = infos['épaisseur']
+    x = coordonnees[0]                  # ou x,y = coordonnees
+    y = coordonnees[1]
+ 
+    feutre = nouveau_stylo(ecriture, fond, epaisseur)
+    deplacer(feutre, x, y)
+    trace_arc(feutre, rayon, angle)
+ 
+    return feutre
+
+
+def carre(cote, infos, coordonnees):
+    ecriture = infos['écriture']
+    fond = infos['fond']
+    epaisseur = infos['épaisseur']
+    x = coordonnees[0]                  # ou x,y = coordonnees
+    y = coordonnees[1]
+    
+    feutre = nouveau_stylo(ecriture, fond, epaisseur)
+    deplacer(feutre, x, y)
+    trace_carre(feutre, cote)
+    
+    
+def rectangle(largeur, hauteur, infos, coordonnees):
+    ecriture = infos['écriture']
+    fond = infos['fond']
+    epaisseur = infos['épaisseur']
+    x = coordonnees[0]                  # ou x,y = coordonnees
+    y = coordonnees[1]
+    
+    feutre = nouveau_stylo(ecriture, fond, epaisseur)
+    deplacer(feutre, x, y)
+    trace_rectangle(feutre, largeur, hauteur)
+    
+    
+def rectangle2(largeur, hauteur, infos, coordonnees):
+    ecriture = infos['écriture']
+    fond = infos['fond']
+    epaisseur = infos['épaisseur']
+    x = coordonnees[0]                  # ou x,y = coordonnees
+    y = coordonnees[1]
+    
+    feutre = nouveau_stylo(ecriture, fond, epaisseur)
+    deplacer(feutre, x, y)
+    trace_rectangle2(feutre, largeur, hauteur)
+
+
+def triangle(largeur, hauteur, infos, coordonnees):
+    ecriture = infos['écriture']
+    fond = infos['fond']
+    epaisseur = infos['épaisseur']
+    x = coordonnees[0]                  # ou x,y = coordonnees
+    y = coordonnees[1]
+    
+    feutre = nouveau_stylo(ecriture, fond, epaisseur)
+    deplacer(feutre, x, y)
+    trace_triangle(feutre, largeur, hauteur, x, y)
+    
+    
+ 
+# Programme principal
+ 
+if __name__ == '__main__':
+ 
+    divers1 = {'écriture':'blue', 'fond':'#FF88FF', 'épaisseur':5}
+    arc_de_cercle(75, 360, divers1, (200,-200))
+    
+    divers2 = {'écriture':'black', 'fond':'blue', 'épaisseur':2}
+    carre(30, divers2, (25,50))
+    
+    divers3 = {'écriture':'black', 'fond':'lime', 'épaisseur':2}
+    rectangle(50, 100, divers3, (-120,100))
+
+    divers4 = {'écriture':'black', 'fond':'#f8ff00', 'épaisseur':2}
+    triangle(70, 80, divers4, (50,100))
+    
+    divers5 = {'écriture':'black', 'fond':'brown', 'épaisseur':2}
+    rectangle2(30, 50, divers5, (100,100))
