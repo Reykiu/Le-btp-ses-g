dc0ebd2708fac539b230f0133a779ad9e63954a8
CringePirate <98109208+CringePirate@users.noreply.github.com>
2022-01-22 18:04:07 +0100

    Update PassionBTP.py

--- PassionBTP.py
+++ PassionBTP.py
@@ -29,7 +29,7 @@ def dessiner_facade(informations):
 def dessiner_porte(informations):
     porte = {}
     porte['écriture'] = 'black'
-    porte['fond'] = black
+    porte['fond'] = 'black'
     porte['épaisseur'] = 2
     
 def dessiner_immeuble(informations:dict):
